032d59d052d1cd1d0c7df02cadf7b0da096ffcb8
ArunSingh <arunsingh.vellore@gmail.com>
2022-01-08 13:11:06 +0530

    udpated code

--- Jan_08/substring.java
+++ Jan_08/substring.java
@@ -1,11 +1,19 @@
 public class substring {
+    //TC: O(N*N)
+    //SC : O(1)
     public static void main(String[] args) {
-        String str = "abc";
+        String str = "abcd";
 
-        
-
-
-
+        for(int i=0;i<str.length();i++){
+            for(int j=i;j<str.length();j++){
+                int k = i;
+                while(k<=j){
+                    System.out.print(str.charAt(k));
+                    k++;
+                }
+                System.out.println();
+            }
+        }
         // ""
         // "a"
         // "b"
