bbb3bbe2f59627dbae2b4d0ff7c3e0e44de0b2db
ArunSingh <arunsingh.vellore@gmail.com>
2022-02-02 21:40:56 +0530

    update

--- PracticeWeek/LamdaExpression/String_vs_char.java
+++ PracticeWeek/LamdaExpression/String_vs_char.java
@@ -1,16 +1,16 @@
-package PracticeWeek.LamdaExpression;
-public class String_vs_char {
-    public static void main(String args[]){
 
-        System.out.println('J' + 'A' + 'V' + 'A');
-        System.out.println("J" + "A" + "V" + "A");
+public class String_vs_char {
+    public static void main(String args[]) {
+
+        // System.out.println('J' + 'A' + 'V' + 'A');
+        // System.out.println("J" + "A" + "V" + "A");
+
         // System.out.println("J" + 'A');
         // System.out.println('A' + "J");
-        // System.out.println('A' + 2.0);
-
+        // System.out.println('A' + 2.0); //double is default for decimals
         // 65.0 + 2.0 => 67.0 => string equivalent
 
-        // System.out.println(2 + 4 + 7 + "Geekster");
+        // System.out.println(2 + 4 + 7 + "Geekster"); // => 2+4+7 = 13 + "Geekster" => "13Geekster"
         // System.out.println("Geekster" + 2 + 4 + 7);
 
     }
--- PracticeWeek/LamdaExpression/lamda.java
+++ PracticeWeek/LamdaExpression/lamda.java
@@ -1,10 +1,8 @@
-package PracticeWeek.LamdaExpression;
 import java.util.*;
 
 public class lamda {
 
     static class comp implements Comparator {
-
         @Override
         public int compare(Object obj1, Object obj2) {
             Integer x = (Integer) obj1;
@@ -15,31 +13,52 @@ public class lamda {
         }
     }
 
+    // Older way
     static class lencomp implements Comparator {
         @Override
         public int compare(Object obj1, Object obj2) {
             String x = (String) obj1;
             String y = (String) obj2;
-            return -1* (x.length() - y.length());
+            return (x.length() - y.length());
         }
     }
 
     public static void main(String[] args) {
         Integer[] arr = { 1, 2, 0, 10, 11, 15, -1, 4 };
 
-        Arrays.sort(arr, (a, b) -> {
-            return -1 * (a - b);
+        // Arrays.sort(arr,new comp());
+        // for (int x : arr) {
+        // System.out.print(x + " ");
+        // }
+
+        // Lamda expression. in java 8 : de-facto way!
+        // Arrays.sort(arr,(x,y)->{
+        // return -1*(x-y);
+        // });
+
+        // for (int x : arr) {
+        // System.out.print(x + " ");
+        // }
+
+        // System.out.println("");
+
+        String[] sa = { "arun", "arun", "singh", "a", "b", "rahul", "katrina" }; // dict order.
+
+        // //The natural/default seq for numbers is ascending order.
+        // // The natural/default seq for strings is dictionary order.
+        // Arrays.sort(sa);
+
+        // Custom defined sorting based len of strings
+        // Arrays.sort(sa,new lencomp());
+
+        // for (String x : sa) {
+        // System.out.print(x + " ");
+        // }
+
+        // Lamda expression. in java 8 : de-facto way!
+        Arrays.sort(sa, (x, y) -> {
+            return -1*(x.length() - y.length());
         });
-
-        for (int x : arr) {
-            System.out.print(x + " ");
-        }
-        System.out.println("");
-
-        String[] sa = { "arun", "singh", "a", "b", "rahul", "katrina" }; // dict order.
-
-        // custom defined sorting based len of strings
-        Arrays.sort(sa, new lencomp());
 
         for (String x : sa) {
             System.out.print(x + " ");
